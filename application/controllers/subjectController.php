fec07f1adc73be1502781b32c0d83996bfafc55c
aljun_alberando <alberando.aljun@gmail.com>
2013-08-08 22:17:50 +0800

    read text file and display its contents

--- application/controllers/subjectController.php
+++ application/controllers/subjectController.php
@@ -8,8 +8,50 @@ class SubjectController extends CI_Controller {
 	
 	public function general_knowledge()
 	{
+		$gen_knowledge = array();
+		$question = "";
+		$choices = array();
+		$counter = 0;
+		$counterTemp = 0;
+		$choice_counter = 0;
 		$generalKnowledge_txtfile = fopen('questions/general_knowledge.txt', 'r');
+		
+		while ($line = fgets($generalKnowledge_txtfile)) {
+			if ($counter == 0) {
+				$question = $line;
+				$counter = $counter + 1;
+			} elseif ($counter == 1) {
+				$choices[$choice_counter] = $line;
+				$choice_counter = $choice_counter + 1;
+				$counter = $counter + 1;
+			} elseif ($counter == 2) {
+				$choices[$choice_counter] = $line;
+				$choice_counter = $choice_counter + 1;
+				$counter = $counter + 1;
+			} elseif ($counter == 3) {
+				$choices[$choice_counter] = $line;
+				$choice_counter = $choice_counter + 1;
+				$counter = $counter + 1;
+			} elseif ($counter == 4) {
+				$choices[$choice_counter] = $line;
+				$choice_counter = $choice_counter + 1;
+				$counter = $counter + 1;
+			} else {
+				$answer = $line;
+				$mathematics[$counterTemp] = array($question, $choices, $answer);
+				$counter = $counter + 1;
+			}
+			if($counter == 6) {
+				$counter = 0;
+				$choice_counter = 0;
+				$choices = array();
+				$counterTemp = $counterTemp + 1;
+			}
+		}
 		fclose($generalKnowledge_txtfile);
+		
+		$data['gen_knowledge'] = $gen_knowledge;
+		$this->load->view('game/gen_knowledge', $data);
 	}
 
 	public function mathematics()
---
+++ application/views/game/general_knowledge.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Interactive Learning Game</title>
+</head>
+<body>
+	<?php
+		 echo $mathematics[0][0];
+		 echo $mathematics[0][1][0];
+		 echo $mathematics[0][1][1];
+		 echo $mathematics[0][1][2];
+	     echo $mathematics[0][1][3];
+	     echo $mathematics[0][2];
+  ?>
+</body>
+</html>
