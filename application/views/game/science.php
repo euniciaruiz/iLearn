1733762922d128191136a782911080e15a742b91
jl_giga <juvylou.giga@g.msuiit.edu.ph>
2013-08-10 19:05:08 +0800

    added the UI for the science view

---
+++ application/views/game/science.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Limelight|Flamenco|Federo|Yesteryear|Josefin Sans|Spinnaker|Sansita One|Handlee|Droid Sans|Oswald:400,300,700" media="screen" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-responsive.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/common.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/fontawesome.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/project.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/main.css" type="text/css" media="screen" charset="utf-8" />
+	<title>Interactive Learning Game</title>
+</head>
+<body>
+    <div id="page-wrapper">
+      <div id="absolute-wrapper">
+        <div class="hero-unit hero-unit-1">
+          <h1 class="heading"></h1>
+          <div class="btns"></div>
+        </div>
+        <div class="hero-unit hero-unit-2">
+          <p>Lorem Ipsum...</p>
+          <div class="btns"></div>
+        </div>
+        <img src="<?php echo base_url();?>images/workinginalaboratory-1.gif" class="imageSci image-1">
+<i class="icon icon-home"></i>
+
+        <button class="btn btn-1 btn-2 btn-4">Button</button>
+        <button class="btn btn-5">Button</button>
+        <button class="btn btn-2 btn-3">Button</button>
+        <img src="<?php echo base_url();?>images/icontelescope.jpg" class="imageSci image-2">
+        <button class="btn btn-1 btn-3">Button</button>
+      </div>
+    </div>
+  </body>
+
+</html>
