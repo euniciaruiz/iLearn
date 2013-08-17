a1d77279e58a70f136b9ebe5cdc6386b1b9d5d1d
aljun_alberando <alberando.aljun@gmail.com>
2013-08-18 00:43:30 +0800

    display question, partial code

--- application/controllers/subjectController.php
+++ application/controllers/subjectController.php
@@ -58,48 +58,25 @@ class SubjectController extends CI_Controller {
 
 	public function mathematics()
 	{
+		$subject_id = $this->input->post('id');
+		$this->load->model('question');
+		$question = array($this->question->getSubjectQuestionList($subject_id));
+		$this->load->model('choice');
+		$choice = $this->choice->getChoiceList();
+	
 		$mathematics = array();
-		$question = "";
 		$choices = array();
-		$counter = 0;
-		$counterTemp = 0;
-		$choice_counter = 0;
-		$mathematics_txtfile = fopen('questions/mathematics.txt', 'r');
-		while ($line = fgets($mathematics_txtfile)) {
-			if ($counter == 0) {
-				$question = $line;
-				$counter = $counter + 1;
-			} elseif ($counter == 1) {
-				$choices[$choice_counter] = $line;
-				$choice_counter = $choice_counter + 1;
-				$counter = $counter + 1;
-			} elseif ($counter == 2) {
-				$choices[$choice_counter] = $line;
-				$choice_counter = $choice_counter + 1;
-				$counter = $counter + 1;
-			} elseif ($counter == 3) {
-				$choices[$choice_counter] = $line;
-				$choice_counter = $choice_counter + 1;
-				$counter = $counter + 1;
-			} elseif ($counter == 4) {
-				$choices[$choice_counter] = $line;
-				$choice_counter = $choice_counter + 1;
-				$counter = $counter + 1;
-			} else {
-				$answer = $line;
-				$mathematics[$counterTemp] = array($question, $choices, $answer);
-				$counter = $counter + 1;
-			}
-			if($counter == 6) {
-				$counter = 0;
-				$choice_counter = 0;
-				$choices = array();
-				$counterTemp = $counterTemp + 1;
-			}
-		}
-		fclose($mathematics_txtfile);  
-		$data['mathematics'] = $mathematics;
-		$this->load->view('game/mathematics', $data);
+		$question_counter = 0;
+		
+		/*for ($i=0; $i < count($question); $i++) {
+			echo $i;
+			//echo element('id', $question);
+			
+		}*/
+		
+			 
+		//$data['mathematics'] = $mathematics;
+		//$this->load->view('game/mathematics', $data);
 	}
 	
 	public function science()
