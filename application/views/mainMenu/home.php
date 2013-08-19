4c4040cf03a8aed0b2733a89b51b12016f6725cf
euniciaruiz <euniciamayeruiz@gmail.com>
2013-08-19 14:19:48 +0800

    edited ui for main menu

--- application/views/mainMenu/home.php
+++ application/views/mainMenu/home.php
@@ -5,13 +5,13 @@
 		<title>Main Menu</title>
 	</head>
 	
-	<body>
+	<body bgcolor="black"><center>
 		
-		<?php echo anchor('subjectController/play', 'Play');?><br>
-		<?php echo anchor('subjectController/help', 'Help');?><br>
-		<?php echo anchor('subjectController/exit', 'Quit');?><br>
+		<?php echo anchor('subjectController/play', '<font face="century gothic" color="#F58EA1" size="32">PLAY</font>');?><br>
+		<?php echo anchor('subjectController/help', '<font face="century gothic" color="#98EDD4" size="32">Help</font>');?><br>
+		<?php echo anchor('subjectController/exit', '<font face="century gothic" color="#E1FC90" size="32">Quit</font>');?><br>
 		
-		
+		</center>
 	</body>
 
 </html>
