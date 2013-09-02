0be1e258dd5575c3fd4a814926fe626459d1ca70
euniciaruiz <euniciamayeruiz@gmail.com>
2013-09-02 21:17:26 +0800

    changed 'play' button to 'start'

--- application/views/mainMenu/home.php
+++ application/views/mainMenu/home.php
@@ -7,7 +7,7 @@
 	
 	<body bgcolor="black"><center>
 		
-		<?php echo anchor('subjectController/play', '<font face="century gothic" color="#F58EA1" size="32">PLAY</font>');?><br>
+		<?php echo anchor('subjectController/play', '<font face="century gothic" color="#F58EA1" size="32">START</font>');?><br>
 		<?php echo anchor('subjectController/help', '<font face="century gothic" color="#98EDD4" size="32">Tutorial</font>');?><br>
 			
 		</center>
